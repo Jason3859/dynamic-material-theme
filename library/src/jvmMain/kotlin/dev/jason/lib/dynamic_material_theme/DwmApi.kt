d850c39009d7ca9dd1f18f929c30e7cb0a420c5b
Jason3859 <augustinej548@gmail.com>
2025-09-15 22:17:54 +0530

    fixed bug with desktop dynamic theme.

--- library/src/jvmMain/kotlin/dev/jason/lib/dynamic_material_theme/DwmApi.kt
+++ library/src/jvmMain/kotlin/dev/jason/lib/dynamic_material_theme/DwmApi.kt
@@ -11,20 +11,13 @@ internal interface DwmApi : Library {
 
 internal fun getWindowsAccentColorLive(): Long? {
     return try {
-
-        if (!System.getProperty("os.name").contains("windows", true)) null
-
         val dwmApi = Native.load("dwmapi", DwmApi::class.java)
         val colorRef = IntByReference()
         val opaqueRef = IntByReference()
         val result = dwmApi.DwmGetColorizationColor(colorRef, opaqueRef)
         if (result == 0) {
-            val abgr = colorRef.value.toLong()
-            val a = (abgr shr 24) and 0xFF
-            val b = (abgr shr 16) and 0xFF
-            val g = (abgr shr 8) and 0xFF
-            val r = abgr and 0xFF
-            (a shl 24 or (r shl 16) or (g shl 8) or b)
+            val argb = colorRef.value.toLong()
+            (argb or 0xFF000000)
         } else null
     } catch (e: Exception) {
         e.printStackTrace()
